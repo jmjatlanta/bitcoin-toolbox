e27e5462031e7264cabfcb8389069fc3aae0b695
John Jones <jmjatlanta@gmail.com>
2019-01-30 14:04:03 -0500

    use varint in tx

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -23,8 +23,7 @@ void transaction::add_input( std::vector<uint8_t>&vec, const input& in)
 void transaction::add_output( std::vector<uint8_t>& vec, const output& out )
 { 
    add( vec, little_endian( out.value, 8 ) );
-   // TODO: this is a variable length integer, convert it!
-   add( vec, little_endian( out.script.size(), 1 ) ); 
+   add( vec, to_varint( out.script.size() ) ); 
    add( vec, out.script );
 }
 
@@ -43,11 +42,10 @@ std::vector<uint8_t> transaction::to_bytes()
    add( ret_val, little_endian( inputs.size(), 1 ) );
    for(auto i : inputs)
       add_input( ret_val, i );
-   // TODO: This is a variable length integer. Convert it!
-   add( ret_val, little_endian( outputs.size(), 1 ) );
+   add( ret_val, to_varint( outputs.size() ) );
    for(auto i : outputs)
       add_output( ret_val, i );
-   add( ret_val, little_endian( witnesses.size(), 1) );
+   add( ret_val, to_varint( witnesses.size() ) );
    for( auto i : witnesses )
       add_witness( ret_val, i );
    add( ret_val, little_endian( locktime, 4) );
